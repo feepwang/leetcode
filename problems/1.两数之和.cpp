7265152bf4a8bd8d8d66225f418c5389b44999f4
feepwang <12470612+feepwang@users.noreply.github.com>
2025-02-08 18:26:51 +0800

    problem 1: twoSum

--- problems/1.两数之和.cpp
+++ problems/1.两数之和.cpp
@@ -11,8 +11,8 @@ using namespace std;
 
 // @lc code=start
 class Solution {
-  public:
-    vector<int> twoSum(vector<int> &nums, int target) {
+   public:
+    vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> mp;
         for (int i = 0; i < nums.size(); i++) {
             if (mp.count(nums[i])) {
