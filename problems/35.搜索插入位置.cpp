37c74d8e73c61ac705bce7802b41e16b5fe155b8
feepwang <12470612+feepwang@users.noreply.github.com>
2025-02-20 16:06:29 +0800

    problem 35: searchInsert

---
+++ problems/35.搜索插入位置.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=cpp
+ *
+ * [35] 搜索插入位置
+ */
+
+#include <vector>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+   public:
+    int searchInsert(vector<int>& nums, int target) {
+        return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+    }
+};
+// @lc code=end
